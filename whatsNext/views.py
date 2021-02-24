56577c8c621f87caa1671bb4fdd4b7a5e7987d02
Travis Walker <traviswalker989@gmail.com>
2021-02-23 22:38:57 -0800

    Default homepage is now releases.html unless Error, which redirects to index.html

--- whatsNext/views.py
+++ whatsNext/views.py
@@ -4,7 +4,6 @@ from .models.data_model import *
 from whatsNext import app
 
 
-@app.route('/', methods=["GET", "POST"])
 @app.route('/index', methods=["GET", "POST"])
 def index():
     # Homepage, evaluate if the request's method is POST.
@@ -22,34 +21,37 @@ def index():
         return render_template("main.html")
 
 
+@app.route('/', methods=["GET", "POST"])
 @app.route('/releases/', methods=["GET", "POST"])
 def releases():
     # Displays table respresenting user's Discogs record collection
-
-    if 'search' in request.form:
-        # If the search button was pressed, pass the form input into the search function
-        # That returns a smaller dataframe of those whose artist or album name matches the search term
-        # We re-render this same page, now only filling the table with that new df
-        results = search(request.form['string'])
-        return render_template(
-            'releases.html',
-            releases=results,
-            search=request.form['string'],
-            username=cache.get("username")
-        )
-    elif 'random' in request.form:
-        # If the 'Random Album' button was pressed,
-        # get a random release_id and pass that to the release() page
-        return redirect(url_for('release', release_id=get_random()))
-    else:
-        # Otherwise a standard GET request, or the 'reset' button was pressed
-        # return the releases page with the full collection df
-        return render_template(
-            'releases.html',
-            releases=get_df(),
-            search='',
-            username=cache.get("username")
-        )
+    try:
+        if 'search' in request.form:
+            # If the search button was pressed, pass the form input into the search function
+            # That returns a smaller dataframe of those whose artist or album name matches the search term
+            # We re-render this same page, now only filling the table with that new df
+            results = search(request.form['string'])
+            return render_template(
+                'releases.html',
+                releases=results,
+                search=request.form['string'],
+                username=cache.get("username")
+            )
+        elif 'random' in request.form:
+            # If the 'Random Album' button was pressed,
+            # get a random release_id and pass that to the release() page
+            return redirect(url_for('release', release_id=get_random()))
+        else:
+            # Otherwise a standard GET request, or the 'reset' button was pressed
+            # return the releases page with the full collection df
+            return render_template(
+                'releases.html',
+                releases=get_df(),
+                search='',
+                username=cache.get("username")
+            )
+    except (KeyError, TypeError) as e:
+        return redirect(url_for('index'))
 
 
 @app.route('/releases/<int:release_id>')
