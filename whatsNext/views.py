1763b630e7e80ecbda95e3c72b2bcb5e410ee429
Travis Walker <traviswalker989@gmail.com>
2022-06-26 10:56:30 -0700

    Fixed color passage

--- whatsNext/views.py
+++ whatsNext/views.py
@@ -60,7 +60,7 @@ def release(release_id):
     try:
         return render_template(
             'release.html',
-            choice=get_dict(release_id),
+            choice=get_dict()[release_id],
             color=get_df()[get_df()['release_id'] == release_id].iloc[0]['cover_color'],
             release_id=release_id,
             similar=get_similar(release_id)
@@ -77,7 +77,7 @@ def color(release_id):
     try:
         return render_template(
             'color.html',
-            choice=get_dict(release_id),
+            choice=get_dict()[release_id],
             color=get_df()[get_df()['release_id'] == release_id].iloc[0]['cover_color'],
             release_id=release_id,
             similar=get_closest_color(release_id)
